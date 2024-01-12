3a0378bbece6440a3a9570330efca237c225b304
Derrick Sual <70816444+Lightnindrummer@users.noreply.github.com>
2024-01-11 20:45:32 -0500

    push base necessary files

---
+++ script.js
@@ -0,0 +1,16 @@
+let contentToReturn; 
+
+function returnContent (page) {  
+
+
+    switch(page){ 
+        case "home": 
+        break; 
+        case "profile": 
+        break; 
+        case "job-search": 
+        break; 
+        default: 
+
+    } 
+}
